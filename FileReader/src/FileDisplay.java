5529a609055a74fe55485710d3cc9ac972e098ae
Anthony Minunni <64501997+Ant-Min@users.noreply.github.com>
2021-10-25 21:59:57 -0400

    Update FileDisplay.java

--- FileReader/src/FileDisplay.java
+++ FileReader/src/FileDisplay.java
@@ -1,23 +1,25 @@
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class FileDisplay {
-  
 	public static Scanner input = null;  
+	static Scanner scnr = new Scanner(System.in);
 	
 	public static void main(String args[]) {
 	  
-	  Scanner scnr = new Scanner(System.in);
 	  boolean run = true;
 	  String choice;
 	  
+	  //while loop that keeps program running until user ends program
 	  while(run) {
 		  
 		  System.out.println("1. MLB Teams\n2. NBA Teams\n3. NFL Teams\n4. NHL Teams");
 		  System.out.println("Enter a number for the corresponding teams or \"0\" to quit");
 		  choice = scnr.nextLine();
 		  
+		  //switch statement for options user has. Most options refer to printTeams method
 		  switch(choice) {
 		  	case "0":
 		  		System.out.println("Goodbye");
@@ -43,9 +45,16 @@ public class FileDisplay {
 	  	  
   }
   
+  //printTeams is a method where the list of sport teams are printed out to the console. The user can then search for a particular team or area
   public static void printTeams(String teams) {
-	  System.out.println(teams + "\n");
 	  
+	  ArrayList<String> list = new ArrayList<String>(); //ArrayList to hold team names
+	  boolean notFound = true; //boolean to test if user entered an existing team
+	  String location; //String where user will enter team name or city
+	  
+	  System.out.println("You chose " + teams + "\n");
+	  
+	  //try-catch block to read from text file of teams
 	  try {
 		  input = new Scanner(new FileInputStream(teams + ".txt"));
 	  }
@@ -54,11 +63,54 @@ public class FileDisplay {
 		  return;
 	  }
 	  
-	  while(input.hasNext()) {
-		  System.out.println(input.nextLine());
+	  //displays teams to user
+	  while(input.hasNextLine()) {
+		  System.out.println(input.nextLine());		  
+	  }
+	  
+	  //Working with only one FileInputStream caused several bugs and other issues, so I reloaded the file
+	  Scanner input2;
+	  try {
+		  input2 = new Scanner(new FileInputStream(teams + ".txt"));
+	  } catch (FileNotFoundException e) {
+		  System.out.println("Problem opening file");
+		  return;
+	  }
+	  
+	  //the new file adds the teams to an ArrayList
+	  while(input2.hasNextLine()) {
+		  list.add(input2.nextLine());
+	  }
+	  
+	  System.out.println("Enter location or team name (proper punctuation), otherwise enter \"0\" to return to menu.");
+	  
+	  location = scnr.nextLine();
+	  
+	  //first if statement prevents the program from crashing because of a nextLine error. Program will return to menu or search for a team depending on user input
+	  if(location.equals(null) || location.equals(" ")|| location.equals("")) {
+		  System.out.println("Nothing was entered. Returning to menu.");
+		  return;
+	  }
+	  else if(location.equals("0")) {
+		  return;
+	  }
+	  else {
+		  for(int i = 0; i < list.size(); i++) {
+			  if(list.get(i).contains(location)) {
+				  notFound = false;
+				  System.out.println(list.get(i));
+			  }
+		  }
+	  }
+	  
+	  if(notFound) {
+		  System.out.println("No teams found");
 	  }
 	  
 	  System.out.println();
+	  
+	  input.close();
+	  input2.close();
   }
 }
 
